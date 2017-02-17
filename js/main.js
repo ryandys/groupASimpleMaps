f0b57b0d43b2ca6b475903a91af1af1d14ff4f83
ryandys <ryan@ourexpressionstudio.com>
2017-02-17 15:11:51 -0500

    initial map files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function() {
+	var map, marker;
+	
+	function initMap() {
+		map = new google.maps.Map(document.querySelector('.map-wrapper'),{
+			center : { lat: 42.983233, lng: -81.250688 },
+			zoom : 14
+		});
+		
+		marker = new google.maps.Marker({
+			position : { lat: 42.983233, lng: -81.250688 },
+			map: map,
+			title: "Hellow World!"
+		});
+	}
+	
+	initMap();
+	
+})();
